3d0ffd929f74f97fd2a45dec339bf0e3ebc5c665
felipekgouvea <felipekinupesg@hotmail.com>
2023-07-25 00:43:21 -0300

    Created Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Header } from './Header/Header.tsx'
 import styles from './App.module.css'
+import { Tasks } from './Tasks/Tasks.tsx'
 
 function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <main></main>
+        <main>
+          <Tasks />
+        </main>
       </div>
     </div>
   )
---
+++ src/Tasks/Tasks.tsx
@@ -0,0 +1,19 @@
+import styles from './Tasks.module.css'
+
+export const Tasks = () => {
+  return (
+    <section className={styles.tasks}>
+      <header className={styles.header}>
+        <div>
+          <p>Tarefas criadas</p>
+          <span>10</span>
+        </div>
+        <div>
+          <p className={styles.textPurple}>Concluídas</p>
+          <span>0 de 10</span>
+        </div>
+      </header>
+      <div className={styles.listTasks}></div>
+    </section>
+  )
+}
